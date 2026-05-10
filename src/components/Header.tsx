a0233b07405650969c49ad71cff61c9fd089eb2b
Annie Huang <annie.hn@gmail.com>
2026-05-10 20:37:06 +1000

    Adding tanstack <Link> into Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
+import { Link } from '@tanstack/react-router';
+
 export default function Header() {
   return (
     <header className='sticky top-0 z-50 border-b border-[var(--line)] bg-[var(--header-bg)] px-4 backdrop-blur-lg'>
-      <h1 className='text-2xl font-bold'>Logo</h1>
+      <Link to='/'>
+        <h1 className='text-2xl font-bold'>Logo</h1>
+      </Link>
+
+      <Link to='/products'>Products</Link>
+      <Link to='/about'>About</Link>
     </header>
   );
 }
