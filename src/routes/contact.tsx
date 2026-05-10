26b174ba0108b6772ef98da85236f34e36b039e4
Annie Huang <annie.hn@gmail.com>
2026-05-10 20:27:43 +1000

    !!!! The content of contact.tsx got automatically generated if you create a new file under routes and the server is running

---
+++ src/routes/contact.tsx
@@ -0,0 +1,10 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+// This file got automatically generated if you create a new file under routes and the server is running
+export const Route = createFileRoute('/contact')({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return <div>Hello "/contact"!</div>;
+}
