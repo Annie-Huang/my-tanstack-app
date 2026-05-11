e048b816587bdd5192f03b3e8944910ebce7041d
Annie Huang <annie.hn@gmail.com>
2026-05-11 20:40:41 +1000

    Get product from Route.useLoaderData() and display the info

--- src/routes.utils.ts
+++ src/routes.utils.ts
@@ -2,7 +2,7 @@ export const sleep = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
 
 export const products = [
-  { id: '1', name: 'Football' },
-  { id: '2', name: 'Hat' },
-  { id: '3', name: 'Cool Shoes' },
+  { id: '1', name: 'Football', description: ' description' },
+  { id: '2', name: 'Hat', description: 'Hat description' },
+  { id: '3', name: 'Cool Shoes', description: 'Cool Shoes description' },
 ];
--- src/routes/product.$id.tsx
+++ src/routes/product.$id.tsx
@@ -10,11 +10,19 @@ export const Route = createFileRoute('/product/$id')({
     await sleep(500);
     // const product = await getProductById(id)
 
-    return products.filter((product) => product.id === id);
+    return { product: products.filter((product) => product.id === id)[0] };
   },
 });
 
 function RouteComponent() {
-  const { id } = Route.useParams();
-  return <div>Product ID: {id}</div>;
+  // const { id } = Route.useParams();
+  // return <div>Product ID: {id}</div>;
+
+  const { product } = Route.useLoaderData();
+  return (
+    <div>
+      <h1 className='text-2xl font-bold'>{product.name}</h1>
+      <p>{product.description}</p>
+    </div>
+  );
 }
